587d16a22e71e3295c10b634883875e3621348bc
Tengku Arif <Arif.TengkuMohd@cohu.com>
2025-01-06 14:31:44 +0800

    feat: remove oil auto cd

--- nvim/lua/atengku/plugins/oil.lua
+++ nvim/lua/atengku/plugins/oil.lua
@@ -13,20 +13,4 @@ return {
       show_hidden = true,
     },
   },
-  config = function(_, opts)
-    require("oil").setup(opts)
-
-    -- Automatically change the working directory when opening a directory in oil.nvim
-    vim.api.nvim_create_autocmd("BufEnter", {
-      callback = function()
-        local oil = require("oil")
-        if vim.bo.filetype == "oil" then -- Check if the current buffer is an oil.nvim buffer
-          local dir = oil.get_current_dir()
-          if dir and #dir > 0 then
-            vim.cmd("cd " .. vim.fn.fnameescape(dir))
-          end
-        end
-      end,
-    })
-  end,
 }
